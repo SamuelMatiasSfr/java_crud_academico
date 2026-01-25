f62b2ab1afd4d9fe8daf9c4486944f024ec2b7a0
Samuel Matias <samuelmatias754@gmail.com>
2026-01-25 20:52:53 -0300

    Adiciona demais paineis no painel principal
    Adiciona painel nome, painel email e painel de botões no painel principal

--- view/JanelaAluno.java
+++ view/JanelaAluno.java
@@ -17,7 +17,7 @@ public class JanelaAluno extends JFrame {
     private JLabel labelMatricula, labelNome, labelEmail;
     private JTextField textoMatricula, textoNome, textoEmail;
     private JButton botaoCreate, botaoUpdate, botaoDelete;
-    private JPanel painelPrincipal, painelMatricula, painelNome, painelEmail;
+    private JPanel painelPrincipal, painelMatricula, painelNome, painelEmail, painelBotoes;
     
     public JanelaAluno(){
         super("Gerenciador Alunos");
@@ -68,24 +68,66 @@ public class JanelaAluno extends JFrame {
 
     private void criarPaneis(){
         criarPainelMatricula();
+        criarPainelNome();
+        criarPainelEmail();
+        criarPainelBotoes();
         criarPainelPrincipal();
         add(painelPrincipal, BorderLayout.WEST);
     }
 
     private void criarPainelMatricula(){
         painelMatricula = new JPanel();
-        painelMatricula.setLayout(new BoxLayout(painelMatricula, BoxLayout.X_AXIS));
-        painelMatricula.add(Box.createHorizontalBox());
+        painelMatricula.setLayout(new BoxLayout(painelMatricula, BoxLayout.Y_AXIS));
+        painelMatricula.add(Box.createVerticalBox());
         painelMatricula.add(labelMatricula);
         painelMatricula.add(Box.createRigidArea(new Dimension(10, 0)));
         painelMatricula.add(textoMatricula);
-        painelMatricula.add(Box.createHorizontalGlue());
+        painelMatricula.add(Box.createVerticalBox());
+    }
+
+    private void criarPainelNome(){
+        painelNome = new JPanel();
+        painelNome.setLayout(new BoxLayout(painelNome, BoxLayout.Y_AXIS));
+        painelNome.add(Box.createVerticalBox());
+        painelNome.add(labelNome);
+        painelNome.add(Box.createRigidArea(new Dimension(10, 0)));
+        painelNome.add(textoNome);
+        painelNome.add(Box.createVerticalBox());
+    }
+
+    private void criarPainelEmail(){
+        painelEmail = new JPanel();
+        painelEmail.setLayout(new BoxLayout(painelEmail, BoxLayout.Y_AXIS));
+        painelEmail.add(Box.createVerticalBox());
+        painelEmail.add(labelEmail);
+        painelEmail.add(Box.createRigidArea(new Dimension(10, 0)));
+        painelEmail.add(textoEmail);
+        painelNome.add(Box.createVerticalBox());
+    }
+
+    private void criarPainelBotoes(){
+        painelBotoes = new JPanel();
+        painelBotoes.setLayout(new BoxLayout(painelBotoes, BoxLayout.X_AXIS));
+        painelBotoes.add(Box.createHorizontalBox());
+        painelBotoes.add(botaoCreate);
+        painelBotoes.add(Box.createRigidArea(new Dimension(10, 0)));
+        painelBotoes.add(botaoUpdate);
+        painelBotoes.add(Box.createRigidArea(new Dimension(10, 0)));
+        painelBotoes.add(botaoDelete);
+        painelBotoes.add(Box.createHorizontalGlue());
     }
 
     private void criarPainelPrincipal(){
         painelPrincipal = new JPanel();
         painelPrincipal.setLayout(new BoxLayout(painelPrincipal, BoxLayout.PAGE_AXIS));
+        painelPrincipal.add(Box.createVerticalBox());
         painelPrincipal.add(painelMatricula, BorderLayout.WEST);
+        painelPrincipal.add(Box.createRigidArea(new Dimension(0, 15)));
+        painelPrincipal.add(painelNome, BorderLayout.WEST);
+        painelPrincipal.add(Box.createRigidArea(new Dimension(0, 15)));
+        painelPrincipal.add(painelEmail, BorderLayout.WEST);
+        painelPrincipal.add(Box.createRigidArea(new Dimension(0, 15)));
+        painelPrincipal.add(painelBotoes, BorderLayout.WEST);
     }
 
     public JButton getBotaoCreate() {
